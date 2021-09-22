dfeedec26d129a4d170cd0df056851379a5dc6a0
Erling Molde <erling.molde@moldesoft.no>
2021-09-23 00:07:39 +0200

    Added more types to test for

--- src/main/java/no/moldesoft/j17/test1/Test.java
+++ src/main/java/no/moldesoft/j17/test1/Test.java
@@ -1,8 +1,13 @@
 package no.moldesoft.j17.test1;
 
 import java.io.Serializable;
+import java.time.*;
+import java.time.format.DateTimeFormatter;
+import java.time.temporal.ChronoUnit;
+import java.time.temporal.Temporal;
 import java.util.Arrays;
 import java.util.List;
+import java.util.Locale;
 
 public class Test {
     public static void main(String[] args) {
@@ -11,7 +16,9 @@ public class Test {
 
     private void exec(String[] args) {
         List<? extends Serializable> list = Arrays.asList(1957, "Erling", null, Math.PI, Long.MAX_VALUE, 3.14f,
-                                                          Something.THREE, 6., 9., GenderPrefix.MISS, GenderPrefix.MR);
+                                                          Something.THREE, 6., 9., GenderPrefix.MISS, GenderPrefix.MR,
+                                                          LocalDate.of(1957, Month.AUGUST, 30), LocalDateTime.now(),
+                                                          ZonedDateTime.now(ZoneId.of("UTC")));
         list.forEach(this::doCheck);
     }
 
@@ -27,9 +34,21 @@ public class Test {
             case Long l -> "Long: " + l;
             case Float f -> "Float: " + f;
             case Double d -> "Double: " + d;
-            case GenderPrefix gp-> gp.getClass().getSimpleName() + '.' + gp;
+            case GenderPrefix gp -> gp.getClass().getSimpleName() + '.' + gp;
             case Enum e -> "Enum: " + e;
+            case Temporal t -> check(t);
             default -> String.format("Some other type : %s, and value %s", obj.getClass(), obj);
         };
+    }
+
+    private String check(Temporal temporal) {
+        Locale locale = Locale.forLanguageTag("nb");
+        if (temporal.isSupported(ChronoUnit.HOURS)) {
+            return "Supports HOURS: " + DateTimeFormatter.ofPattern("d. MMM uuuu 'kl.' H:mm:ss").format(temporal);
+        }
+        if (temporal.isSupported(ChronoUnit.DAYS)) {
+            return "Supports DAYS: " + DateTimeFormatter.ofPattern("d. MMM uuuu", locale).format(temporal);
+        }
+        return temporal.getClass().getSimpleName();
     }
 }
