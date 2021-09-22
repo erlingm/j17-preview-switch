dce800ea4fcebb22ebe51ff44e6a99c92e849c7d
Erling Molde <erling.molde@moldesoft.no>
2021-09-23 00:59:57 +0200

    Style adjustments. Added example output to README.

--- src/main/java/no/moldesoft/j17/test1/Test.java
+++ src/main/java/no/moldesoft/j17/test1/Test.java
@@ -2,11 +2,14 @@ package no.moldesoft.j17.test1;
 
 import java.time.*;
 import java.time.format.DateTimeFormatter;
+import java.time.temporal.ChronoField;
 import java.time.temporal.ChronoUnit;
 import java.time.temporal.Temporal;
 import java.util.Arrays;
 import java.util.List;
 import java.util.Locale;
+import java.util.Optional;
+import java.util.function.Predicate;
 
 public class Test {
     public static void main(String[] args) {
@@ -14,10 +17,30 @@ public class Test {
     }
 
     private void exec(String[] args) {
-        List<Object> list = Arrays.asList(1957, "Erling", null, Math.PI, Long.MAX_VALUE, 3.14f,
-                                          Something.THREE, 6., 9., GenderPrefix.MISS, GenderPrefix.MR,
-                                          LocalDate.of(1957, Month.AUGUST, 30), LocalDateTime.now(),
-                                          ZonedDateTime.now(ZoneId.of("UTC")));
+        Optional<ZoneId> first = ZoneId.getAvailableZoneIds()
+                .stream()
+                .filter(((Predicate<String>) zone2 -> zone2.contains(
+                        "Hawaii")).or(zone1 -> zone1.contains("United States"))
+                                .or(zone -> zone.contains("New York")))
+                .map(ZoneId::of)
+                .findFirst();
+        ZoneId hawaii = first.orElseGet(ZoneId::systemDefault);
+        ZoneId honolulu = ZoneId.of("Pacific/Honolulu");
+        List<Object> list = Arrays.asList(1957,
+                                          "Erling",
+                                          null,
+                                          Math.PI,
+                                          Long.MAX_VALUE,
+                                          3.14f,
+                                          Something.THREE,
+                                          6.,
+                                          GenderPrefix.MISS,
+                                          GenderPrefix.MR,
+                                          LocalDate.of(1957, Month.AUGUST, 30),
+                                          LocalDateTime.now(),
+                                          ZonedDateTime.now(ZoneId.of("UTC")),
+                                          Instant.now().atZone(hawaii),
+                                          Instant.now().atZone(honolulu));
         list.forEach(this::doCheck);
     }
 
@@ -42,11 +65,17 @@ public class Test {
 
     private String check(Temporal temporal) {
         Locale locale = Locale.forLanguageTag("nb");
+        if (temporal.isSupported(ChronoUnit.HOURS) && temporal.isSupported(ChronoField.OFFSET_SECONDS)) {
+            return "Supports HOURS: " +
+                   DateTimeFormatter.ofPattern("d. MMM uuuu 'kl.' H:mm:ss - VV - zzzz", locale).format(temporal);
+        }
         if (temporal.isSupported(ChronoUnit.HOURS)) {
-            return "Supports HOURS: " + DateTimeFormatter.ofPattern("d. MMM uuuu 'kl.' H:mm:ss").format(temporal);
+            return "Supports HOURS: " +
+                   DateTimeFormatter.ofPattern("d. MMM uuuu 'kl.' H:mm:ss").format(temporal);
         }
         if (temporal.isSupported(ChronoUnit.DAYS)) {
-            return "Supports DAYS: " + DateTimeFormatter.ofPattern("d. MMM uuuu", locale).format(temporal);
+            return ", AND Supports DAYS: " +
+                   DateTimeFormatter.ofPattern("d. MMM uuuu", locale).format(temporal);
         }
         return temporal.getClass().getSimpleName();
     }
